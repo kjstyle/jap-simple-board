8c5de9ed7a0d492bf57967c424b7bdd07dfe260d
kjstyle <kjstyle79@naver.com>
2020-06-18 02:02:03 +0900

    @BeforeEach에서 공통 given (공통으로 사용될 픽스쳐 생성) 처리 테스트해봄

--- src/test/java/com/kjstyle/jpaboard/service/UserServiceMockTest.java
+++ src/test/java/com/kjstyle/jpaboard/service/UserServiceMockTest.java
@@ -6,6 +6,7 @@ import com.kjstyle.jpaboard.domain.user.User;
 import com.kjstyle.jpaboard.domain.user.UserRepository;
 import com.kjstyle.jpaboard.exceptions.NoSuchUserException;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -20,11 +21,18 @@ class UserServiceMockTest extends BaseTest {
     @InjectMocks
     private UserService userService;
 
+    private User mockUser;
+
+    @BeforeEach
+    void setMockObject() {
+        this.mockUser = User.builder().userId("kj").email("kj@naver.com").id(1L).name("이길주").build();
+        given(userRepository.findById(anyLong())).willReturn(java.util.Optional.ofNullable(mockUser));
+    }
+
     @Test
     public void 회원조회_테스트() {
         // given
-        User mockUser = User.builder().userId("kj").email("kj@naver.com").id(1L).name("이길주").build();
-        given(userRepository.findById(anyLong())).willReturn(java.util.Optional.ofNullable(mockUser));
+        // beforeEach에서 수행
 
         // when
         User user = userService.findById(1L).orElseThrow(() -> new NoSuchUserException());
