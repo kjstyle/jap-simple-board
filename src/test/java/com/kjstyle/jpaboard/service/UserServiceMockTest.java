8d163bdfe596af02f918ccdbf3d2d078828dd42d
kjstyle <kjstyle79@naver.com>
2020-06-18 01:58:15 +0900

    junit5에서 mockito로 목테스트 샘플 만들어보기 요 다음은 의미있는 로직을 넣어 만들어봐야...

--- src/test/java/com/kjstyle/jpaboard/service/UserServiceMockTest.java
+++ src/test/java/com/kjstyle/jpaboard/service/UserServiceMockTest.java
@@ -2,7 +2,34 @@ package com.kjstyle.jpaboard.service;
 
 
 import com.kjstyle.jpaboard.common.BaseTest;
+import com.kjstyle.jpaboard.domain.user.User;
+import com.kjstyle.jpaboard.domain.user.UserRepository;
+import com.kjstyle.jpaboard.exceptions.NoSuchUserException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 
 class UserServiceMockTest extends BaseTest {
+    @Mock
+    private UserRepository userRepository;
 
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void 회원조회_테스트() {
+        // given
+        User mockUser = User.builder().userId("kj").email("kj@naver.com").id(1L).name("이길주").build();
+        given(userRepository.findById(anyLong())).willReturn(java.util.Optional.ofNullable(mockUser));
+
+        // when
+        User user = userService.findById(1L).orElseThrow(() -> new NoSuchUserException());
+
+        // then
+        Assertions.assertEquals(mockUser.getEmail(), user.getEmail());
+    }
 }
